ff07159eeefac2154640960a2bdbf0b03dee4e09
Wojciech Tomaszewski <w.tomaszewski92@gmail.com>
2018-03-14 18:50:13 +0100

    Project init

---
+++ src/main/java/wt/connectfourgame/ConnectFourGameTddApplication.java
@@ -0,0 +1,12 @@
+package wt.connectfourgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectFourGameTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectFourGameTddApplication.class, args);
+	}
+}
